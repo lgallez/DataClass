f08f347606eb837e6da4f59bbfd08e3bdee5c6fb
Layla G <lgallez@outlook.com>
2019-01-24 17:10:33 -0800

    added pymongo

--- Web Scrape/app.py
+++ Web Scrape/app.py
@@ -1,30 +1,31 @@
-from flask import Flask, render_template, jsonify, redirect
-from flask_pymongo import PyMongo
+from flask import Flask, render_template
+from flask_pymongo import pymongo
 import scrape_mars
+from pymongo import MongoClient
+
 
 # create instance of Flask app
 app = Flask(__name__)
-mars = scrape_mars
 
-# mongo = PyMongo(app)
+# create mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = pymongo(app)
 
-#  create route that renders index.html template
+client = pymongo.MongoClient()
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+db = client.mars_db
+collection = db.mars_input
+
 @app.route("/")
-def index():
-    
-    return render_template("index.html", mars=mars)
+def home():
+    mars_data = list(db.collection.find())[0]
+    return render_template('index.html', mars_data=mars_data)
 
 @app.route("/scrape")
-def scrape():
-
+def mars_scrape():
     mars_data = scrape_mars.scrape()
-    mars.update(
-        {},
-        mars_data,
-        upsert=True
-    )
-    return redirect("http://localhost:5000/", code=302)
-
+    db.collection.insert_one(mars_data)
+    return render_template('scrape.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
