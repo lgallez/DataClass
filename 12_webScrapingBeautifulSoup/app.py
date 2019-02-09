4b415efc14cd686cf923a33cddc047d2c6cb194e
Layla G <lgallez@outlook.com>
2019-02-09 11:36:40 -0800

    name change

--- 12_webScrapingBeautifulSoup/app.py
+++ 12_webScrapingBeautifulSoup/app.py
@@ -2,26 +2,26 @@
 from flask import Flask, render_template
 import pymongo
 import scrape
-
+import scrape_mars
 # create instance of Flask app
 app = Flask(__name__)
-
+conn = 'mongod://localhost:27017'
 # create mongo connection
-client = pymongo.MongoClient()
+client = pymongo.MongoClient(conn)
 db = client.mars_db
 collection = db.mars_data_entries
 
 @app.route("/")
 def home():
-mars_data = list(db.collection.find())[0]
-return  render_template('index.html', mars_data=mars_data)
+    mars_data = list(db.collection.find())[0]
+    return  render_template('index.html', mars_data=mars_data)
 
 @app.route("/scrape")
 def web_scrape():
-db.collection.remove({})
-mars_data = scrape.scrape()
-db.collection.insert_one(mars_data)
-return  render_template('scrape.html')
+    db.collection.remove({})
+    mars_data = scrape.scrape()
+    db.collection.insert_one(mars_data)
+    return  render_template('scrape.html')
 
 if __name__ == "__main__":
-app.run(debug=True)
+    app.run(debug=True)
